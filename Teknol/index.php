2be782bb7f003f32b4cfbfeca3e77b896f2d0dc2
pyrofefa <pyro_fefa@hotmail.com>
2016-07-03 17:16:46 -0700

    Update index.php

--- Teknol/index.php
+++ Teknol/index.php
@@ -3,11 +3,26 @@
 
 	$sql="select * from comentarios";
 	$resultado=mysql_query($sql);
-	while($row=mysql_fetch_assoc($resultado))
-	{
-		$arr[]=$row;	
-	}
-		$json=json_encode($arr);
-
-	echo $json;
 ?>
+<table>
+	<tr>
+    	<td>Id</td>
+		<td>Nombre</td>
+		<td>Correo</td>
+		<td>Comentario</td>
+	<tr>
+<?php
+	while($rw=mysql_fetch_array($resultado))
+	{
+?>
+	<tr>
+    	<td> <?php echo ($id=$rw['id']);?></td>
+		<td> <?php echo ($nombre=$rw['nombre']); ?></td>
+		<td> <?php echo ($correo=$rw['correo']); ?></td>
+		<td> <?php echo ($comentario=$rw['comentario']); ?></td>
+	</tr>
+
+<?php
+	} 
+?>
+</table>
